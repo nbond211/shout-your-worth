126dc64dabc1fd104774fcce49ef0de5748d22fb
Nick Bond <bond.n@husky.neu.edu>
2019-03-20 16:56:34 -0400

    installs sass

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `The Shout Your Worth Team`,
   },
   plugins: [
+    `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,12 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import IndexStyles from "./index.module.scss";
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Shout Your Worth is a toolkit to help you communicate your value.</h1>
+    <h1 className={IndexStyles.title}>Shout Your Worth is a toolkit to help you communicate your value.</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
   </Layout>
