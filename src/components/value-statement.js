73c35a21c8aa0898192039f6920b3c94f8cd5e14
maryats <maryats@users.noreply.github.com>
2019-03-27 11:16:00 -0400

    rename to value statements

---
+++ src/components/value-statement.js
@@ -0,0 +1,87 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { graphql, StaticQuery } from "gatsby"
+import styles from "../styles/testimonials.module.css"
+
+class ValueStatement extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      a: 0,
+      s: 0,
+      r: 0.
+    }
+
+    this.randomizeValueStatement = this.randomizeValueStatement.bind(this);
+  }
+
+  randomizeValueStatement() {
+    console.log("update", this.props.data.accomplishments);
+    let a = this.props.data.accomplishments ? this.props.data.accomplishments.length : 1;
+    let s = this.props.data.skills ? this.props.data.skills.length : 1;
+    let r = this.props.data.results ? this.props.data.results.length : 1;
+
+    this.setState({ 
+      a: Math.floor(Math.random() * a),
+      s: Math.floor(Math.random() * s),
+      r: Math.floor(Math.random() * r)
+    });
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {this.randomizeValueStatement();}, 3000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    let accomplishment = this.props.data.accomplishments ? this.props.data.accomplishments[this.state.a] : "built something cool";
+    let skill = this.props.data.skills ? this.props.data.skills[this.state.s] : "bravery";
+    let result = this.props.data.results ? this.props.data.results[this.state.r] : "felt 30% more confident";
+
+    return <div className={styles.testimonials}>
+      <h2>So this one time I...</h2>
+      <div className={styles.value_statement}>
+        <Statement title="accomplishment" value={accomplishment} />
+        <p className={styles.joiner}>using my</p>
+        <Statement title="skill" value={skill}/>
+        <p className={styles.joiner}>and as a result,</p>
+        <Statement title="result" value={result} />
+      </div>
+    </div>;
+  } 
+}
+
+function Statement(props) {
+  return <div className={styles.statement}>
+    <h3 className={styles.value}>{props.value}</h3>
+    <h5 className={styles.title}>{props.title}</h5>
+  </div>;
+}
+
+Statement.propTypes = {
+  title: PropTypes.string,
+  value: PropTypes.string
+}
+
+export default props => (
+  <StaticQuery
+    query={graphql`
+      query {
+        allDataJson {
+          edges {
+            node {
+              accomplishments,
+              skills,
+              results
+            }
+          }
+        }
+      }
+    `}
+    render={data => <ValueStatement data={data.allDataJson.edges[0].node} {...props} />}
+  />
+);
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Layout from "../components/layout"
 import CTA from "../components/cta"
 import FAQ from "../components/faq"
 import SEO from "../components/seo"
-import Testimonials from "../components/testimonials"
+import ValueStatement from "../components/value-statement"
 
 import IndexStyles from "./index.module.scss";
 
@@ -14,7 +14,7 @@ const IndexPage = () => (
     <h1 className={IndexStyles.title}>Shout Your Worth is a toolkit to help you communicate your value.</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <Testimonials />
+    <ValueStatement />
     <CTA />
     <FAQ />
   </Layout>
