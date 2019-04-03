da622f518b417770c780e94bdd24f858e6887c11
Nick Bond <bond.n@husky.neu.edu>
2019-04-03 09:47:05 -0400

    adds and positions header elements

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,17 @@
 import React from "react"
 import styles from "../styles/header.module.css"
 
-export default function Header(props) {
-  return <div className={styles.container}>
-    <div className={styles.logo} />
-  </div>;
+export default function Header() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.logoContainer}>
+        <div className={styles.logo}/>
+        <div className={styles.divider}/>
+        <h1 className={styles.slogan}>A toolkit to help you communicate your value</h1>
+      </div>
+      <div className={styles.topLeftShape}/>
+      <div className={styles.bottomLeftShape}/>
+      <div className={styles.bottomRightShape}/>
+    </div>
+  );
 }
