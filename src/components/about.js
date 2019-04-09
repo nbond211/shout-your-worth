0ceeb31c697c1a9a10254e4ab64d2908e002cee2
Nick Bond <bond.n@husky.neu.edu>
2019-04-09 15:07:32 -0400

    adds responsive styles for cards for all screen sizes

--- src/components/about.js
+++ src/components/about.js
@@ -13,20 +13,22 @@ export default function About(props) {
       <h5 className={styles.smallTitle}>How Does It Work?</h5>
       <h1 className={styles.subtitle}>Practice talking about the value that you bring to the table in an approachable, fun setting.</h1>
     </div>
-    <div className={styles.col_2}>
-      <img className={`${styles.accomplishment35} ${styles.card}`} src={accomplishment35}/>
-      <img className={`${styles.skill25} ${styles.card}`} src={skill25}/>
-      <img className={`${styles.result19} ${styles.card}`} src={result19}/>
-      <img className={`${styles.skill22} ${styles.card}`} src={skill22}/>
-      <img className={`${styles.accomplishment34} ${styles.card}`} src={accomplishment34}/>
-    </div>
-    <div className={styles.about}>
-      <div className={styles.col_1}>
-        <h3>Why Shout Your Worth?</h3>
-        <p>A playful, game-like experience creates a safe space where players feel supported and comfortable being vulnerable and brave.<br/><br/>
-          Players walk away from the experience feeling confident and empowered to tackle what can be an uncomfortable conversation – advocating for themselves.<br/><br/></p>
-        <h3>What are Value Statements?</h3>
-        <p>Value statements are a proven method for communicating your worth. They can be used in conversation that requires self-advocacy such as a salary negotiation or job interview.</p>
+    <div className={styles.contentContainer}>
+      <div className={styles.col_2}>
+        <img className={`${styles.accomplishment35} ${styles.card}`} src={accomplishment35}/>
+        <img className={`${styles.skill25} ${styles.card}`} src={skill25}/>
+        <img className={`${styles.result19} ${styles.card}`} src={result19}/>
+        <img className={`${styles.skill22} ${styles.card}`} src={skill22}/>
+        <img className={`${styles.accomplishment34} ${styles.card}`} src={accomplishment34}/>
+      </div>
+      <div className={styles.about}>
+        <div className={styles.col_1}>
+          <h3>Why Shout Your Worth?</h3>
+          <p>A playful, game-like experience creates a safe space where players feel supported and comfortable being vulnerable and brave.<br/><br/>
+            Players walk away from the experience feeling confident and empowered to tackle what can be an uncomfortable conversation – advocating for themselves.<br/><br/></p>
+          <h3>What are Value Statements?</h3>
+          <p>Value statements are a proven method for communicating your worth. They can be used in conversation that requires self-advocacy such as a salary negotiation or job interview.</p>
+        </div>
       </div>
     </div>
   </div>;
