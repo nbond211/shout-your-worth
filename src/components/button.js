ab10618465560553897dffdb3755842b1acb8a83
Nick Bond <bond.n@husky.neu.edu>
2019-04-10 10:05:05 -0400

    renames button component to Button

--- src/components/button.js
+++ src/components/button.js
@@ -1,7 +1,7 @@
 import React from "react"
 import styles from "../styles/button.module.css"
 
-export default function PhotoStrip(props) {
+export default function Button(props) {
   return (
     <a href={props.href} target="_blank">
       <button className={styles.button}>{props.children}</button>
