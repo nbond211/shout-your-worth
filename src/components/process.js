93e9aceb36cf62d95c95e5201be8f3d25d064e08
Nick Bond <bond.n@husky.neu.edu>
2019-04-14 18:21:12 -0400

    adds and styles arrows and numbers

--- src/components/process.js
+++ src/components/process.js
@@ -27,9 +27,18 @@ class Process extends React.Component {
           />
           <div className={styles.textContainer}>
             <div className={styles.titleContainer}>
-              <h3>{current.title}</h3>
+              <h3 className={styles.title}>{current.title}</h3>
+              <div className={styles.stageToggleContainer}>
+                <svg className={styles.arrow} width='5' height='9' xmlns='http://www.w3.org/2000/svg'>
+                  <path d='M4.286.047l.67.712L1.392 4.5l3.562 3.74-.67.713L.046 4.5z' fill='#2D2D2D'/>
+                </svg>
+                <span className={styles.number}>{this.state.index + 1} / 3</span>
+                <svg className={styles.arrow} width='5' height='9' xmlns='http://www.w3.org/2000/svg'>
+                  <path d='M.714.047l-.67.712L3.608 4.5.045 8.24l.67.713L4.954 4.5z' fill='#2D2D2D'/>
+                </svg>
+              </div>
             </div>
-            <p>{current.description}</p>
+            <p className={styles.description}>{current.description}</p>
           </div>
         </div>
       </div>
