254d1453ea97df5be5d6f9feb9e1fe6b1b563f7b
Nick Bond <bond.n@husky.neu.edu>
2019-04-14 21:36:19 -0400

    hooks up click events for navigating through stages

--- src/components/process.js
+++ src/components/process.js
@@ -8,10 +8,15 @@ class Process extends React.Component {
 
     this.state = { index: 0 }
     this.advance = this.advance.bind(this);
+    this.previous = this.previous.bind(this);
   }
 
   advance() {
     this.setState({index: (this.state.index + 1) % this.props.data.length})
+  }
+
+  previous() {
+    this.setState({index: (this.state.index - 1) % this.props.data.length})
   }
 
 
@@ -29,11 +34,11 @@ class Process extends React.Component {
             <div className={styles.titleContainer}>
               <h3 className={styles.title}>{current.title}</h3>
               <div className={styles.stageToggleContainer}>
-                <svg className={styles.arrow} width='5' height='9' xmlns='http://www.w3.org/2000/svg'>
+                <svg onClick={this.previous} className={styles.arrow} width='5' height='9' xmlns='http://www.w3.org/2000/svg'>
                   <path d='M4.286.047l.67.712L1.392 4.5l3.562 3.74-.67.713L.046 4.5z' fill='#2D2D2D'/>
                 </svg>
                 <span className={styles.number}>{this.state.index + 1} / 3</span>
-                <svg className={styles.arrow} width='5' height='9' xmlns='http://www.w3.org/2000/svg'>
+                <svg onClick={this.advance} className={styles.arrow} width='5' height='9' xmlns='http://www.w3.org/2000/svg'>
                   <path d='M.714.047l-.67.712L3.608 4.5.045 8.24l.67.713L4.954 4.5z' fill='#2D2D2D'/>
                 </svg>
               </div>
